281eced2ba8d40db2a343ff6be05e8268989b1ec
coderalitutorials <razamuneer670@gmail.com>
2026-07-06 01:57:42 -0700

    text added

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("SuperGuard Rotherhithe Backend Server Running");
+  res.send("SuperGuard Rotherhithe Backend Server Running safely");
 });
 
 /* ==========================================================
